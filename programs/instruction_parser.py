50ed48ef4163444ff42539727f142c9b8bb3207e
lzt0901 <519230944@qq.com>
2016-11-24 19:49:01 -0500

    Update Boot Program
    Fixed a bug where GETS and PUTS defined in boot program would output
    '\0', which is an invisible character but would cause copy and paster
    not to behave as expected.

--- programs/instruction_parser.py
+++ programs/instruction_parser.py
@@ -44,8 +44,8 @@ opcode_dict = {
             'JGE': 017,
             }
 
-with open('Program 2') as in_file:
-    with open('Program 2.txt', 'w') as sys.stdout:
+with open('Boot Program') as in_file:
+    with open('Boot Program.txt', 'w') as sys.stdout:
         while True:
             line = in_file.readline()
             if not line:
